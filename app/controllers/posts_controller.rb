31e41c2ff56cee3916bc5b1570c608948457d1ca
yamazaki <yyuta750@gmail.com>
2023-07-31 01:11:46 +0000

    CRUD処理実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,27 +8,39 @@ class PostsController < ApplicationController
   end
   
   def create
-    @post = Post.new(params.require(:post).permit(:start_at, :end_at, :is_all_day, :title, :content))
+    @post = Post.new(params.require(:post).permit(:title, :start_at, :end_at, :is_all_day, :content))
     if @post.save
+      flash[:success] = "スケジュール作成に成功しました"
       redirect_to posts_path
     else
+      flash[:failure] = "スケジュール作成に失敗しました"
       render "new"
     end
   end
   
   def show
-    
+    @post = Post.find(params[:id])
   end
   
   def edit
-    
+    @post = Post.find(params[:id])
   end
   
   def update
-    
+    @post = Post.find(params[:id])
+    if @post.update(params.require(:post).permit(:title, :start_at, :end_at, :is_all_day, :content))
+      flash[:success] = "編集に成功しました"
+      redirect_to posts_path
+    else
+      flash[:failure] = "編集に失敗しました"
+      render "edit"
+    end
   end
   
   def destroy
-    
+    @post = Post.find(params[:id])
+    @post.destroy
+    flash[:success] = "スケジュールを削除しました"
+    redirect_to :posts
   end
 end
